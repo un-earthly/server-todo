b7e8aa54da288dd2b3d60db14d0e616ff3ed0300
un-earthly <vijayalamin@gmail.com>
2022-05-18 15:23:00 -0700

    baackend ready

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require('mongodb');
 const port = process.env.PORT || 80
 const app = express()
 app.use(cors())
@@ -14,13 +14,23 @@ async function run() {
         await client.connect();
         const todoCollection = client.db("todoDB").collection("todo")
         app.get('/todo', async (req, res) => {
-            res.send(todoCollection.find().toArray())
+            res.send(await todoCollection.find({ email: req.query.user }).toArray())
         })
         app.post('/todo', async (req, res) => {
             res.send(await todoCollection.insertOne(req.body))
         })
+        app.put('/todo/:id', async (req, res) => {
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    completed: true
+                },
+            };
+            res.send(await todoCollection.updateOne({ _id: ObjectId(req.params.id) }, updateDoc, options))
+
+        })
         app.delete('/todo/:id', async (req, res) => {
-            res.send(await todoCollection.deleteOne(ObjectId(req.params.id)))
+            res.send(await todoCollection.deleteOne({ _id: ObjectId(req.params.id) }))
         })
 
     } finally {
